6f956a68d406f7ef5d80eeaddf6d55470a182990
FRCSola <Sola.dugbo@barstowschool.org>
2020-01-18 14:52:43 -0600

    Initial Commit

---
+++ InfiniteRecharge2020/src/main/java/com/frcteam1939/infiniterecharge2020/robot/OI.java
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.frcteam1939.infiniterecharge2020.robot;
+
+import com.frcteam1939.infiniterecharge2020.util.Gamepad;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class OI {
+  public final Joystick left = new Joystick(0);
+  public final Joystick right = new Joystick(1);
+  public final Gamepad gamepad = new Gamepad(2);
+}
---
+++ InfiniteRecharge2020/src/main/java/com/frcteam1939/infiniterecharge2020/robot/RobotMap.java
@@ -0,0 +1,23 @@
+
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.frcteam1939.infiniterecharge2020.robot;
+
+public class RobotMap {
+  
+  // Talons/Victors
+
+  // Solenoids
+
+  // Analog Input
+
+  // Digital Input
+
+  // Digital Output
+
+}
