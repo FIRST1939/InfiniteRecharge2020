2e61b8c3e65e6f675ad769defa26a429473d4fe9
EliChase <59711935+EliChase@users.noreply.github.com>
2020-01-28 17:12:59 -0600

    Update RobotMap.java
    shooter motors and clearer map

--- InfiniteRecharge2020/src/main/java/com/frcteam1939/infiniterecharge2020/robot/RobotMap.java
+++ InfiniteRecharge2020/src/main/java/com/frcteam1939/infiniterecharge2020/robot/RobotMap.java
@@ -11,11 +11,19 @@ package com.frcteam1939.infiniterecharge2020.robot;
 public class RobotMap {
   
   // Talons/Victors
+
+      //Intake
   public static final int intakeRollersTalon = 0;
+
+      //Drivetrain
   public static final int leftBackTalon = 0;
   public static final int leftFrontTalon = 0;
   public static final int rightBackTalon = 0;
   public static final int rightFrontTalon = 0;
+
+      //Shooter
+  public static final int shooter1 = 0;
+  public static final int shooter2 = 0;
 
   // Solenoids
   public static final int PCM = 0;
