c7d4b692e2a31e66e3ad964fe60204793b79c1d1
EliChase <59711935+EliChase@users.noreply.github.com>
2020-01-30 17:12:38 -0600

    declaring all motors and solenoids
    title

--- InfiniteRecharge2020/src/main/java/com/frcteam1939/infiniterecharge2020/robot/RobotMap.java
+++ InfiniteRecharge2020/src/main/java/com/frcteam1939/infiniterecharge2020/robot/RobotMap.java
@@ -11,23 +11,36 @@ package com.frcteam1939.infiniterecharge2020.robot;
 public class RobotMap {
   
   // Talons/Victors
- 
   public static final int intakeRollersTalon = 0;
 
-  public static final int indexTalon1 = 0;
-  public static final int indexTalon2 = 0;
-     
-  public static final int leftBackTalon = 0;
-  public static final int leftFrontTalon = 0;
-  public static final int rightBackTalon = 0;
-  public static final int rightFrontTalon = 0;
+  // Solenoids
+    public static final int indexTalon1 = 0;
+    public static final int indexTalon2 = 0;
 
-  public static final int shooterTalon1 = 0;
-  public static final int shooterTalon2 = 0;
+    public static final int leftBackTalon = 0;
+    public static final int leftFrontTalon = 0;
+    public static final int rightBackTalon = 0;
+    public static final int rightFrontTalon = 0;
+
+    public static final int turretTalon = 0;
+
+    public static final int shooterTalon1 = 0;
+    public static final int shooterTalon2 = 0;
+
+    public static final int controlPanelRedline = 0;
+
+    public static final int climberTalon = 0;
+    public static final int gondolaSnowblower = 0;
 
   // Solenoids
-  public static final int PCM = 0;
-  public static final int intakeSolenoid = 0;
+    public static final int PCM = 0;
+
+    public static final int intakeSolenoid = 0;
+
+    public static final int drivetrainSolenoid = 0;
+
+    public static final int shooterSolenoid1 = 0;
+    public static final int shooterSolenoid2 = 0;
 
   // Analog Input
 
