5b1ef5c3293145c27b43c49c6dde5870d105983e
FRCSola <sola.dugbo@barstowschool.org>
2020-02-03 12:19:57 -0600

    ADDED: Indexer Commands
    added indexer code, needs to be tuned with actual robot.

--- InfiniteRecharge2020/src/main/java/com/frcteam1939/infiniterecharge2020/robot/subsystems/Indexer.java
+++ InfiniteRecharge2020/src/main/java/com/frcteam1939/infiniterecharge2020/robot/subsystems/Indexer.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,18 +7,18 @@
 
 package com.frcteam1939.infiniterecharge2020.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.frcteam1939.infiniterecharge2020.robot.RobotMap;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+public class Indexer extends SubsystemBase {
+  /**
+   * Creates a new Indexer.
+   */
+  public Indexer() {
 
-public class Indexer extends Subsystem {
-  private TalonSRX talonAcross = new TalonSRX(RobotMap.indexTalon1);
-  private TalonSRX talonUp = new TalonSRX(RobotMap.indexTalon2);
+  }
 
   @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+  public void periodic() {
+    // This method will be called once per scheduler run
   }
 }
