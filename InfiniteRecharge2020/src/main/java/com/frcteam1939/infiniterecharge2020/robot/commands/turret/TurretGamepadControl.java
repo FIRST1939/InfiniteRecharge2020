ab597d1f19295668e3bd0b481bc2095aed17e2c8
FRCSola <sola.dugbo@barstowschool.org>
2020-02-12 21:36:24 -0600

    Fixed issues in Master
    FIXED:  updated librarys, changed victors to talons, renamed talonFX to Falcons

---
+++ InfiniteRecharge2020/src/main/java/com/frcteam1939/infiniterecharge2020/robot/commands/turret/TurretGamepadControl.java
@@ -0,0 +1,14 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.frcteam1939.infiniterecharge2020.robot.commands.turret;
+
+/**
+ * Add your docs here.
+ */
+public class TurretGamepadControl {
+}
