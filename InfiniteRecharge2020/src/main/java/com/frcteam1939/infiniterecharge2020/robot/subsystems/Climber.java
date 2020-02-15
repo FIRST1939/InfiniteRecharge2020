d8ac8771865683f2bf64735cdde7c0eedbf3f069
FRCSola <sola.dugbo@barstowschool.org>
2020-02-15 00:35:26 -0600

    ADDED: Climber Commands

--- InfiniteRecharge2020/src/main/java/com/frcteam1939/infiniterecharge2020/robot/subsystems/Climber.java
+++ InfiniteRecharge2020/src/main/java/com/frcteam1939/infiniterecharge2020/robot/subsystems/Climber.java
@@ -1,40 +1,24 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
 package com.frcteam1939.infiniterecharge2020.robot.subsystems;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.frcteam1939.infiniterecharge2020.robot.RobotMap;
-import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Climber extends Subsystem {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-  private TalonSRX climberTalon = new TalonSRX(RobotMap.climberFalcon);
-  private TalonSRX gondolaTalon = new TalonSRX(RobotMap.gondolaTalon);
+public class Climber extends SubsystemBase {
+  /**
+   * Creates a new Climber.
+   */
+  public Climber() {
+
+  }
 
   @Override
-  public void initDefaultCommand() {
-
-  }
-
-  //Brake Mode
-  public void enableBrakeModeClimber(){
-    climberTalon.setNeutralMode(NeutralMode.Brake);
-  }
-
-  public void disableBrakeModeClimber(){
-    climberTalon.setNeutralMode(NeutralMode.Coast);
-  }
-
-  public void enableBrakeModeGondola(){
-    gondolaTalon.setNeutralMode(NeutralMode.Brake);
-  }
-
-  public void disableBrakeModeGondola(){
-    gondolaTalon.setNeutralMode(NeutralMode.Coast);
+  public void periodic() {
+    // This method will be called once per scheduler run
   }
 }
